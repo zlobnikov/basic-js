599469204a2e7b1806b91a7bbd06614e0dedd93f
zlobnikov <daffodil@list.ru>
2021-09-14 12:40:37 +0300

    init: implement carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+
+  sampleActivity = Number(sampleActivity);
+  if (sampleActivity !== sampleActivity || !sampleActivity) return false;
+
+  const dividend = Math.log(MODERN_ACTIVITY / sampleActivity);
+  const divider = Math.log(2) / HALF_LIFE_PERIOD;
+  const time = Math.ceil(dividend / divider);
+
+  return time >= 0 ? time : false;
 }
